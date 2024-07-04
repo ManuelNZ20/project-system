be9cc88a4343a17e369dfb695ecdaf38106c4664
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-07-03 21:23:56 -0500

    init project

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  // Dashboard administrativo usando gráficos e tabelas
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
